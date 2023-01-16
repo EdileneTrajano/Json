efca40f3a4db6641c25e2d0844009672d6901199
EdileneTrajano <114194953+EdileneTrajano@users.noreply.github.com>
2023-01-16 19:39:24 -0300

    Delete server.mjs

--- server.mjs
+++
@@ -1,9 +0,0 @@
-import jsonServe from 'jsonServe';
-const server = jsonServe.create();
-const router = jsonServe.router('db.json');
-const middlewres = jsonServe.defaults();
-const port = process.env.PORT || 3200;
-
-server.use(middlewres);
-
-server.use(router);
