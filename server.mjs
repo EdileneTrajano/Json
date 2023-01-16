3e3163d6d6b985666707fa3f4608155a996f75cf
EdileneTrajano <114194953+EdileneTrajano@users.noreply.github.com>
2023-01-16 19:27:52 -0300

    MOD 3

---
+++ server.mjs
@@ -0,0 +1,9 @@
+import jsonServe from 'jsonServe';
+const server = jsonServe.create();
+const router = jsonServe.router('db.json');
+const middlewres = jsonServe.defaults();
+const port = process.env.PORT || 3200;
+
+server.use(middlewres);
+
+server.use(router);
